892bc54e7f6cc707256b72999113c746eab88681
Alejandro Lopez <alejandro@umbra3d.com>
2020-12-18 11:39:21 +0200

    Add day 18 solutions

---
+++ 18.2.cc
@@ -0,0 +1,109 @@
+#include <cstdio>
+#include <cctype>
+#include <iostream>
+#include <cmath>
+#include <string>
+#include <vector>
+#include <set>
+#include <map>
+#include <queue>
+#include <unordered_set>
+#include <unordered_map>
+#include <algorithm>
+#include <inttypes.h>
+
+using namespace std;
+
+constexpr int64_t kMaxLen = 1024;
+
+char line[kMaxLen + 1];
+int cur;
+
+/*
+ * expr := factor mfactors
+ * mfactors := * factor mfactors | eps
+ * factor := term mterms
+ * mterms := + term mterms | eps
+ * term := int | ( expr )
+ */
+
+void SkipWs()
+{
+    while (line[cur] == ' ')
+        ++cur;
+}
+
+char Peek()
+{
+    SkipWs();
+    return line[cur];
+}
+
+char Eat()
+{
+    SkipWs();
+    return line[cur++];
+}
+
+int64_t expr();
+
+int64_t term()
+{
+    int64_t res = 0;
+    if (isdigit(Peek()))
+        res = Eat() - '0';
+    else if (Peek() == '(')
+    {
+        assert(Eat() == '(');
+        res = expr();
+        assert(Eat() == ')');
+    }
+    return res;
+}
+
+int64_t mterms(int64_t acc)
+{
+    if (Peek() == '+')
+    {
+        assert(Eat() == '+');
+        acc += term();
+        return mterms(acc);
+    }
+    return acc;
+}
+
+int64_t factor()
+{
+    return mterms(term());
+}
+
+int64_t mfactors(int64_t acc)
+{
+    if (Peek() == '*')
+    {
+        assert(Eat() == '*');
+        acc *= factor();
+        return mfactors(acc);
+    }
+    return acc;
+}
+
+int64_t expr()
+{
+    return mfactors(factor());
+}
+
+int main()
+{
+    ios::sync_with_stdio(0), cin.tie(0);
+
+    int64_t ans = 0;
+    for (; fgets(line, kMaxLen, stdin);)
+    {
+        cur = 0;
+        ans += expr();
+    }
+    cout << ans << '\n';
+
+    return 0;
+}
